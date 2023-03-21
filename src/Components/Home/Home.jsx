eaf9d181e02196b23c34c61fb042c5d0ac0c1343
Abdul Ragib <abdulragib36@gmail.com>
2023-03-21 15:02:53 +0530

    sidebar done

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,7 +3,14 @@ import './Home.css';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="Home">
+       <div className='left'>
+        <button className="btn">
+            CONNECT WALLET
+        </button>
+        </div>
+       <div className='right-h'>Right side</div>
+    </div>
   )
 }
 
