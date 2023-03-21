f06ee90dcb959c4fde05a4a41a68b4f8fe14a3d4
Abdul Ragib <abdulragib36@gmail.com>
2023-03-21 15:25:53 +0530

    Home Right side done

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -9,7 +9,12 @@ const Home = () => {
             CONNECT WALLET
         </button>
         </div>
-       <div className='right-h'>Right side</div>
+       <div className='right-h'>
+        <h1 className="home-Title">MetaPoll</h1>
+        <span>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br>
+           Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,<br></br>
+            when an unknown printer took a galley</span>
+       </div>
     </div>
   )
 }
