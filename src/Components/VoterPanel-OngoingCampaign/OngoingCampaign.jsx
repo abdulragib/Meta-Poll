fc5616531890afabf998c512e960ec750ccbdc8a
Abdul Ragib <abdulragib36@gmail.com>
2023-05-10 21:09:33 +0530

    Voter -> OngoingCampaign Content Added

--- src/Components/VoterPanel-OngoingCampaign/OngoingCampaign.jsx
+++ src/Components/VoterPanel-OngoingCampaign/OngoingCampaign.jsx
@@ -1,26 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import "./OngoingCampaign.css";
 const OngoingCampaign = () => {
+  const [selectedCandidate, setSelectedCandidate] = useState("");
+
+  const handleCandidateSelection = (e) => {
+    setSelectedCandidate(e.target.value);
+  };
   return (
     <div className="ongoing-campaign">
       <div className="left">
         <button className="btn">WALLET ADDRESS</button>
       </div>
       <div className="right-oc">
-          <h1 className="home-Title">MetaPoll</h1>
-          <h2 className="right-oc-name">Name</h2>
-          <div className="right-oc-data">
-            <div className="right-oc-left">
-              <button className="btn btn-right">Candidate 1</button>
-              <button className="btn btn-right">Candidate 2</button>
-              <button className="btn btn-right">Candidate 3</button>
-              <button className="btn btn-right">Candidate 4</button>
+        <h1 className="home-Title">MetaPoll</h1>
+        <h2 className="right-oc-name">CR NOMINATION D2007</h2>
+        <div className="right-oc-data">
+          <div className="right-oc-left">
+            <button className="btn btn-right">Candidate 1</button>
+            <button className="btn btn-right">Candidate 2</button>
+            <button className="btn btn-right">Candidate 3</button>
+            <button className="btn btn-right">Candidate 4</button>
           </div>
           <div className="right-oc-right">
-            <input type='checkbox' className="right-oc-right-item"></input>
-            <input type='checkbox' className="right-oc-right-item"></input>
-            <input type='checkbox' className="right-oc-right-item"></input>
-            <input type='checkbox' className="right-oc-right-item"></input>
+            <input
+              type="checkbox"
+              className="right-oc-right-item"
+              value="Candidate 1"
+              checked={selectedCandidate === "Candidate 1"}
+              onChange={handleCandidateSelection}
+            ></input>
+            <input
+              type="checkbox"
+              className="right-oc-right-item"
+              value="Candidate 2"
+              checked={selectedCandidate === "Candidate 2"}
+              onChange={handleCandidateSelection}
+            ></input>
+            <input
+              type="checkbox"
+              className="right-oc-right-item"
+              value="Candidate 3"
+              checked={selectedCandidate === "Candidate 3"}
+              onChange={handleCandidateSelection}
+            ></input>
+            <input
+              type="checkbox"
+              className="right-oc-right-item"
+              value="Candidate 4"
+              checked={selectedCandidate === "Candidate 4"}
+              onChange={handleCandidateSelection}
+            ></input>
           </div>
         </div>
         <button className="btn right-oc-vote">Vote</button>
